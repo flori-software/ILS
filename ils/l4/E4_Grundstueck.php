257c512f787720fd49f3998ac6b0f8e13e7a7972
Arkadiusz Paluszek <contact@flori-software.de>
2022-02-26 21:41:09 +0100

    Aufgaben Lektion 4

--- ils/l4/E4_Grundstueck.php
+++ ils/l4/E4_Grundstueck.php
@@ -19,35 +19,69 @@ div {
 <h1>Grundstückspreise</h1>';
 
 if($auswerten == 1) {
-    $breite     = $_POST["breite"] ?? 1;
-    $laenge     = $_POST["laenge"] ?? 1;
-    $preis      = $_POST["preis"] ?? 1;
-    $provision  = $_POST["provision"] ?? 3;
-    $ust        = $_POST["ust"] ?? 0;
+    $breite             = $_POST["breite"];
+    $laenge             = $_POST["laenge"];
+    $preis              = $_POST["preis"];
+    $provision_prozent  = $_POST["provision_prozent"];
+    // Da die leere Checkbox keinen Wert übermnittelt, wird für diesen Fall die 0 der Variable $ust zugeordnet 
+    $ust                = $_POST["ust"] ?? 0;
 
     // Berechnung weiterer Werte
     $netto                = $breite * $laenge * $preis;
-    $provision            = $netto * $provision / 100;
+    $provision            = $netto * $provision_prozent / 100;
     $netto_incl_provision = $netto + $provision;
-    $brutto               = $netto_incl_provision * 1.19;
-
+    if($ust == 1) {
+        $brutto = $netto_incl_provision * 1.19;
+    } else {
+        $brutto = $netto_incl_provision;
+    }
+    
     // Ausgabe der Ergebnisse
-
+    echo '<table border="0">
+        <tr>
+            <td>Breite:</td>
+            <td>'.$breite.' m</td>
+        </tr>
+        <tr>
+            <td>Länge:</td>
+            <td>'.$laenge.' m</td>
+        </tr>
+        <tr>
+            <td>Nettopreis:</td>
+            <td>'.zahl_de($preis).' €</td>
+        </tr>
+        <tr>
+            <td>Nettopreis mit Provision:</td>
+            <td>'.zahl_de($netto_incl_provision).' €</td>
+        </tr>
+        <tr>
+            <td>Bruttopreis:</td>
+            <td>'.zahl_de($brutto).' €</td>
+        </tr>
+    </table>';
 } else {
     // Wenn die Variablenwerte nicht zuvor per POST übergeben wurden, werden die Variablen an dieser Stelle initiiert
-
+    $breite               = 1;
+    $laenge               = 1;
+    $netto                = 1;
+    $preis                = 1;
+    $provision_prozent    = 3;
+    $netto_incl_provision = 1.03; // Eine Berechnung macht bei vorgegebenen Werten keinen Sinn
+    // Wenn keine Werte übergeben wurden, ist die Checkbox für Ust nicht angeklickt, es wird also keine Ust berechnet
+    $ust                  = 0;
+    $brutto               = $netto_incl_provision;
 }
 
 echo '<form action="'.$_SERVER["PHP_SELF"].'?auswerten=1" method="POST">
 <div>';
 // Ein Grundstück unter 1m x 1m macht keinen Sinn, deshgalb die min-Angabe
 // Ein Grundstückpreis unter 1 wäre schön, aber in diesem Universum nicht zu finden
-input_number(id: "breite", label: "Breite: ", platzhalter: "Breite?", step: 1, min: 1);
-input_number(id: "laenge", label: "Länge: ", platzhalter: "Länge?", step: 1, min: 1);
-input_number(id: "preis", label: "Preis pro qm", platzhalter: "Preis / qm?", step: 0.01, min: 1);
+input_number(id: "breite", label: "Breite: ", platzhalter: "Breite?", step: 1, min: 1, wert: $breite);
+input_number(id: "laenge", label: "Länge: ", platzhalter: "Länge?", step: 1, min: 1, wert: $laenge);
+input_number(id: "preis", label: "Preis pro qm", platzhalter: "Preis / qm?", step: 0.01, min: 1, wert: $preis);
 $optionen = Array(3, 4, 5, 6, 7);
-dynAuswahl(id: "provision", name: "provision", options: $optionen, multiple: false, label: "Provisionssatz: ");
-eineCheckbox(id: "ust", name: "ust", label: "Umsatzsteuer", value: 1, vergleichswert: 0);
+dynAuswahl(id: "provision_prozent", name: "provision_prozent", options: $optionen, multiple: false, label: "Provisionssatz: ");
+eineCheckbox(id: "ust", name: "ust", label: "Umsatzsteuer", value: 1, vergleichswert: $ust);
 echo '</div>
 <p>
 <input type="submit" value="Berechnen">
--- ils/l4/functions.php
+++ ils/l4/functions.php
@@ -1,29 +1,34 @@
 <?php
-function dynAuswahl(string $id, string $name, array $options, bool $multiple, $label = "") {
-    echo '<p>';
+function dynAuswahl(string $id, string $name, array $options, bool $multiple, $label = "", $wert = "") {
+    echo "<p>\n";
     if($label != "") {
-        echo '<label for="'.$id.'">'.$label.'</label>';
+        echo "<label for='".$id."'>".$label."</label>";
     }
-    echo '<select name="'.$name;
+    echo "<select name='".$name;
     // Eckige Klammer für die Übermittlung der Ergebnisse als Array
-    if($multiple) {echo '[]';}
-    echo '" id="'.$id.'" ';
+    if($multiple) {echo "[]";}
+    echo "' id='".$id."' ";
     // Hier wird das Auswahlfeld ggf. zum Auswahlfeld mit Mehrfachauswahl
-    if($multiple) {echo 'multiple';}
-    echo '>';
+    if($multiple) {echo "multiple";}
+    echo ">\n";
     foreach ($options as $option) {
-        echo '<option value="'.$option.'">'.$option.'</option>';
+        echo "\t<option value='".$option."' ";
+        if($option == $wert) {echo "selected";}
+        echo ">".$option."</option>\n";
     }
-    echo '</select>
-    </p>';
+    echo "</select>\n
+    </p>\n";
 }
 
-function input_number($id, $label, $platzhalter, $step, $min = 0) {
+function input_number($id, $label, $platzhalter, $step, $min = 0, $wert = 0) {
     echo '<p>
     <label for="'.$id.'">'.$label.'</label>
     <input type="number" id="'.$id.'" name="'.$id.'" step="'.$step.'" placeholder="'.$platzhalter.'" ';
     if($min != 0) {
         echo 'min="'.$min.'"';
+    }
+    if($wert != 0) {
+        echo 'value="'.$wert.'"';
     }
     echo '>
     </p>';
@@ -34,5 +39,16 @@ function eineCheckbox($id, $name, $label, $value, $vergleichswert) {
     if($vergleichswert == $value) {echo 'checked';}
     echo '>
     <label for="'.$id.'">'.$label.'</label>';
+}
+
+function zahl_de($zahl, $tausendertrennzeichen = true) {
+	// Die Zahl wird vom Computerformat ins deutsche Format umgewandelt
+	$zahl = floatval($zahl);
+	if($tausendertrennzeichen == true) {
+		$zahl = number_format($zahl,2,",",".");
+	} else {
+		$zahl = number_format($zahl,2,",","");
+	}
+	return $zahl;
 }
 ?>
