01e27e9bc3425d94aa017910287aceb6b4c8159d
Arkadiusz Paluszek <contact@flori-software.de>
2022-10-02 00:13:55 +0200

    Abschluß Lektion 8

--- ils/l8/index.php
+++ ils/l8/index.php
@@ -6,7 +6,7 @@ include "klassen.php";
 // Daten holen
 $daten_zeigen = $_GET["daten_zeigen"] ?? 0;
 $region       = $_POST["region"] ?? 0;
-if($daten_zeigen == 1) {
+if($daten_zeigen == 1 && $region != 0) {
     $region_id     = $_POST["region"] ?? 0;
     $laender       = new laender(region_id: $region_id);
     $meine_laender = $laender->get_laender();
@@ -20,7 +20,7 @@ echo '<html lang="de">
 <style>
     .my_title {
         position: relative;
-        left: 30%;
+        left: 20%;
         font-family: Helvetica;
         font-weight: lighter;
         font-size: 36px;
@@ -29,17 +29,20 @@ echo '<html lang="de">
 
     .normal {
         position: relative;
-        left: 30%;
+        left: 20%;
         font-family: Helvetica;
         font-weight: lighter;
         font-size: 16px;
     }
     select {
         position: relative;
-        left: 24%;
+        left: 19%;
         font-family: Helvetica;
         font-weight: lighter;
         font-size: 20px;
+    }
+    td {
+        width: 20%
     }
 </style>
 </head>
@@ -51,13 +54,19 @@ Bitte wählen Sie die Region aus, deren Daten angezeigt werden sollen:<br>
 <p><select name="region">';
 my_selectField(tabelle: "regions", angezeigter_inhalt: "name", werte: "region_id", treffer: $region);
 echo '</select></p>
-</form></div>
-<div class="normal" style="top: 50px;">
-<table>
-<tr><th>Land</th><th>Fläche</th><th>AVG BIP</th><th>AVG Bevölkerung</th><th>Kontinent</th></tr>';
-foreach ($laender as $land) {
-    echo '<tr><td>'.$land->land.'</td><td>'.$land->flaeche.'</td></tr>';
+</form></div>';
+if(isset($meine_laender)) {
+    echo '<div class="normal" style="top: 50px;">
+    <table>
+    <tr style="font-weight: bold;"><td>Land</td><td>Fläche</td><td>AVG BIP</td><td>AVG Bevölkerung</td><td>Kontinent</td></tr>';
+    foreach ($meine_laender as $land) {
+        echo '<tr><td>'.$land->land.'</td><td>'.$land->flaeche.'</td><td>'.$land->avg_bip.'</td>
+        <td>'.$land->avg_bevoelkerung.'</td><td>'.$land->kontinent.'</td></tr>';
+        // 
+    }
 }
+
+
 echo '</table>
 </div>';
 
--- ils/l8/klassen.php
+++ ils/l8/klassen.php
@@ -14,14 +14,23 @@ class laender {
     public function __construct($region_id) {
         $pdo = myDatabaseConnection();
         $this->region_id = $region_id;
-        $sql = "SELECT * FROM countries WHERE region_id=".$this->region_id;
+        $sql = "SELECT countries.name as name, area, AVG(gdp) as avg_gdp, AVG(population) as avg_population,  continents.name as continent FROM countries 
+        INNER JOIN country_stats ON countries.country_id = country_stats.country_id
+        INNER JOIN regions ON countries.region_id = regions.region_id 
+        INNER JOIN continents ON regions.continent_id = continents.continent_id
+        WHERE countries.region_id=".$this->region_id."
+        GROUP BY country_stats.country_id";
 
+        $this->laender = Array();
         if($stmt = $pdo->prepare($sql)) {
             $stmt->execute();
             while($row = $stmt->fetch()) {
-                $land          = new land;
-                $land->land    = $row["name"];
-                $land->flaeche = $row["area"];
+                $land                   = new land;
+                $land->land             = $row["name"];
+                $land->flaeche          = $row["area"];
+                $land->avg_bip          = $row["avg_gdp"];
+                $land->avg_bevoelkerung = $row["avg_population"];
+                $land->kontinent        = $row["continent"];
                 $this->laender[] = $land;
             }
         }
